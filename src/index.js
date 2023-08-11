2cca148c21fb9ca838bc197095984e7ff64500bf
Sonu Raut <sonu16122001@gmail.com>
2023-08-11 12:28:15 +0530

    add firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import 'firebase/compat/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCPNx36E-nQR6_xIvgrw8FK1YoMcr_IIVY",
+  authDomain: "cart-b548b.firebaseapp.com",
+  projectId: "cart-b548b",
+  storageBucket: "cart-b548b.appspot.com",
+  messagingSenderId: "884470586264",
+  appId: "1:884470586264:web:5288c9f4bf85bb5cc069ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
